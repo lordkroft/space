1a8dd4cf5f61d1fb02280a48a8baca2fa78daf0a
Vladimir <4402po@gmail.com>
2021-11-09 16:28:38 +0300

    test5

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,3 @@ echo "Vova DevoPes!"
 echo "122345"
 echo "test1"
 
-#GIT_COMMIT=$(git rev-parse --short HEAD)
-#echo ${GIT_COMMIT}
-
-
-#environment {
-#        GIT_COMMIT_SHORT = sh(
-#                script: "printf \$(git rev-parse --short ${GIT_COMMIT})",
-#                returnStdout: true
-#        )
-#    }
